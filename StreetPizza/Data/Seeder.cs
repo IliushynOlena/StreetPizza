f0b32421be20ee373dc63695ca55ebcbc1879ecf
spn337 <spn337@gmail.com>
2020-05-04 13:25:56 +0300

    finish menu-page

--- StreetPizza/Data/Seeder.cs
+++ StreetPizza/Data/Seeder.cs
@@ -14,8 +14,8 @@ namespace StreetPizza.Data
             SeedProducts(context, new Product
             {
                 Name = "Маргарита",
-                Img = "1margarita.jpg",
-                Ingredients = "Помідори, томатний соус, моцарелла, базилік",          
+                Img = "1margarita.png",
+                Ingredients = "Соус томатний, сир Моцарелла і свіжі томати",          
                 PriceMedium = 125,
                 PriceLarge = 175,
                 IsFavorite = true
@@ -24,8 +24,9 @@ namespace StreetPizza.Data
             SeedProducts(context, new Product
             {
                 Name = "Чотири сезона",
-                Img = "pitstsa-chetyre-sezona.jpg",
-                Ingredients = "Помідори, печериці, салямі, сир Дор Блю, моцарелла, томатний соус",
+                Img = "pitstsa-chetyre-sezona.png",
+                Ingredients = "Соус томатний, сир Моцарелла, стиглі томати, печериці," +
+                " апетитна салямі, сир Дор Блю, що просто тане в роті.",
                 PriceMedium = 155,
                 PriceLarge = 255,
                 IsFavorite = true
@@ -34,14 +35,59 @@ namespace StreetPizza.Data
             SeedProducts(context, new Product
             {
                 Name = "Вегетеріанська",
-                Img = "1vegetarianskaya.jpg",
-                Ingredients = "Помідори, печериці, моцарелла, солодкий перець, " +
-                "кукурудза, маслини, маринована синя цибуля, томатний соус, свіжа зелень",
+                Img = "1vegetarianskaya.png",
+                Ingredients = "Соус томатний, сир Моцарелла, печериці, стиглі томати, " +
+                "солодкий перець, солодка кукурудза, маринований синій цибуля, маслини " +
+                "і свіжа зелень.",
                 PriceMedium = 159,
                 PriceLarge = 259,
                 IsFavorite = true
             });
 
+            SeedProducts(context, new Product
+            {
+                Name = "Мюнхенська",
+                Img = "myunhenskaya.png",
+                Ingredients = "Гірчичний соус, сир моцарелла, апетитна салямі, " +
+                "маринована цибуля, мисливські ковбаски а також копчене куряче філе, " +
+                "трохи зелені і пікантне часникове масло.",
+                PriceMedium = 169,
+                PriceLarge = 269,
+                IsFavorite = false
+            });
+
+            SeedProducts(context, new Product
+            {
+                Name = "Тревізо",
+                Img = "trevizo.png",
+                Ingredients = "Гірчичний соус, сир моцарелла, апетитна салямі, " +
+                "свинина, мисливські ковбаски, стиглі томати, мариновані огірки, " +
+                "гарячий гострий перець, трохи зелені і пікантне часникове масло.",
+                PriceMedium = 169,
+                PriceLarge = 269,
+                IsFavorite = false
+            });
+
+            SeedProducts(context, new Product
+            {
+                Name = "Пеппероні",
+                Img = "pepperoni.png",
+                Ingredients = "Соус томатний, сир Моцарелла в поєднанні з ковбаскою Пепероні.",
+                PriceMedium = 179,
+                PriceLarge = 279,
+                IsFavorite = false
+            });
+
+            SeedProducts(context, new Product
+            {
+                Name = "Сімейна",
+                Img = "semeynaya.png",
+                Ingredients = "Соус томатний, сир Моцарелла, соковита шинка, " +
+                "солодка кукурудза, печериці, копчене куряче філе, свіжа зелень.",
+                PriceMedium = 169,
+                PriceLarge = 269,
+                IsFavorite = false
+            });
             #endregion
         }
 
