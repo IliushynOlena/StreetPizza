abf8a7c6b7813b72c1a4179db538f415683c06c4
spn337 <spn337@gmail.com>
2020-05-05 11:20:50 +0300

    create pagination(without style)

---
+++ StreetPizza/ViewModels/PaggingInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreetPizza.ViewModels
+{
+    public class PaggingInfo
+    {
+        //всього товарів в базі
+        public int TotalItems { get; set; }
+        //всього товарів на сторінці
+        public int ItemsPerPage { get; set; }
+        //номер поточної сторінки
+        public int CurrentPage { get; set; }
+        //загальна кількість сторінок
+        public int TotalPages => 
+            (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    }
+}
