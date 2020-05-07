c2a7e52b67b1f740ecbb6ed81ac130e72ba4e136
spn337 <spn337@gmail.com>
2020-05-07 13:09:26 +0300

    show categoty-menu

---
+++ StreetPizza/Components/CategoriesMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StreetPizza.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreetPizza.Components
+{
+    public class CategoriesMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+        public CategoriesMenuViewComponent(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
