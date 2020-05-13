b7fb4759abe4858f3d7760b6667bc1891c9d7463
Maksyutynskyi Roman <maksutroman@gmail.com>
2020-05-13 20:17:36 +0300

    add view edit

---
+++ StreetPizza/ViewModels/EditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreetPizza.ViewModels
+{
+    public class EditViewModel : CreateProductsViewModel
+    {
+        public int Id { get; set; }
+        public string ExistImgPath { get; set; }
+
+    }
+}
